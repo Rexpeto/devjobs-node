09b4fe4c48a8078ab5becc565e858c6cbdef34ad
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-24 16:02:03 -0400

    Se agregó el modelo de vacantes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
+//? Modelos
+import Vacantes from "../models/Vacantes.js";
+
 export const db =  () => {
     mongoose.connect(process.env.DATABASES, {userNewUrlParser: true});
     mongoose.connection.on('error', error => console.log(error));
