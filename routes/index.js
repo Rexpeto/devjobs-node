28582566ba1c180f92ecb6d825183e8734d8371a
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-24 15:30:16 -0400

    Se agregó la ruta de nuevas vacantes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { mostrarTrabajos } from '../controllers/homeControllers.js';
+import { formVacantes } from '../controllers/vacantesControllers.js';
 const router = express.Router();
 
 router.get('/', mostrarTrabajos);
+
+//? Crear vacantes
+router.get('/vacantes/nueva', formVacantes);
 
 export default router;
