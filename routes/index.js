66d86f935207d6f0b3007c8d6f9399648feabb45
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-27 17:02:26 -0400

    Se agregó la ruta para editar vacante

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { mostrarTrabajos } from '../controllers/homeControllers.js';
-import { crearVacante, formVacantes, mostrarVacante } from '../controllers/vacantesControllers.js';
+import { crearVacante, editarVacante, formVacantes, mostrarVacante } from '../controllers/vacantesControllers.js';
 const router = express.Router();
 
 router.get('/', mostrarTrabajos);
@@ -11,5 +11,8 @@ router.post('/vacantes/nueva', crearVacante);
 
 //? Mostrar Vacante
 router.get('/vacantes/:url', mostrarVacante);
+
+//? Editar vacante
+router.get('/vacantes/edit/:url', editarVacante);
 
 export default router;
