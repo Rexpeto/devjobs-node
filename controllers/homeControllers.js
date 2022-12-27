b9bc5717dcf80ff3c8b6c30238b608bc8d07e005
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-27 00:33:21 -0400

    Consulta a la tabla vacantes

--- controllers/homeControllers.js
+++ controllers/homeControllers.js
@@ -1,9 +1,18 @@
+import {Vacantes} from '../models/Vacantes.js'
+
 //? Muestra los trabajos en el home
-export const mostrarTrabajos = (req, res) => {
+export const mostrarTrabajos = async (req, res, next) => {
+    //* Obtener vacantes
+    const vacantes = await Vacantes.find().lean();
+
+    if(!vacantes) return next();
+
+
     res.render('home', {
         pagina: 'DevJobs',
         tagLine: 'Encuentra y pública trabajos para Developers',
         barra: true,
-        boton: true
+        boton: true,
+        vacantes
     });
 }
