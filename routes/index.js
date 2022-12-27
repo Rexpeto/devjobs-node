9b580991e79da663d516a3c5cccadebcc577d366
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-27 00:59:51 -0400

    Ruta para mostrar vacante

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { mostrarTrabajos } from '../controllers/homeControllers.js';
-import { crearVacante, formVacantes } from '../controllers/vacantesControllers.js';
+import { crearVacante, formVacantes, mostrarVacante } from '../controllers/vacantesControllers.js';
 const router = express.Router();
 
 router.get('/', mostrarTrabajos);
@@ -8,5 +8,8 @@ router.get('/', mostrarTrabajos);
 //? Crear vacantes
 router.get('/vacantes/nueva', formVacantes);
 router.post('/vacantes/nueva', crearVacante);
+
+//? Mostrar Vacante
+router.get('/vacantes/:url', mostrarVacante);
 
 export default router;
