8285786077ed8aecafd1745e0bcf1d2d6192a0bd
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-27 00:32:44 -0400

    Consulta a la tabla vacantes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { mostrarTrabajos } from '../controllers/homeControllers.js';
-import { formVacantes } from '../controllers/vacantesControllers.js';
+import { crearVacante, formVacantes } from '../controllers/vacantesControllers.js';
 const router = express.Router();
 
 router.get('/', mostrarTrabajos);
 
 //? Crear vacantes
 router.get('/vacantes/nueva', formVacantes);
+router.post('/vacantes/nueva', crearVacante);
 
 export default router;
