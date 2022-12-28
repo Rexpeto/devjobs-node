1a48b34d2a59987a50677c458d2593bdd8fb35c9
Rexpeto <Carlosgallardoparra@hotmail.com>
2022-12-27 23:10:41 -0400

    Rutas de register

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { register, storageRegister } from '../controllers/usuarioControllers.js';
 import { mostrarTrabajos } from '../controllers/homeControllers.js';
 import { crearVacante, editarVacante, formVacantes, guardarEditVacante, mostrarVacante } from '../controllers/vacantesControllers.js';
 const router = express.Router();
@@ -15,5 +16,9 @@ router.get('/vacantes/:url', mostrarVacante);
 //? Editar vacante
 router.get('/vacantes/edit/:url', editarVacante);
 router.post('/vacantes/edit/:url', guardarEditVacante);
+
+//? Register
+router.get('/register', register);
+router.post('/register', storageRegister);
 
 export default router;
